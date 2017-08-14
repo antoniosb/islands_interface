eb3608f05fd9a25d6e8b6f527707b2c294a01320
Antonio Britto <antonioabritto@gmail.com>
2017-08-14 17:11:37 -0300

    playing with game channel and the "hello" funcion handler

--- lib/islands_interface_web/channels/game_channel.ex
+++ lib/islands_interface_web/channels/game_channel.ex
@@ -6,4 +6,60 @@ defmodule IslandsInterface.GameChannel do
   def join("game:" <> _player, _payload, socket) do
     {:ok, socket}
   end
+
+  def handle_in("hello", payload, socket) do
+    #    {:reply, {:ok, payload}, socket}
+    #
+    #    payload = %{message: "We forced this error."}
+    #    {:reply, {:error, payload}, socket}
+    #
+    #    push(socket, "said_hello", payload)
+    #    {:noreply, socket}
+    broadcast!(socket, "said_hello", payload)
+    {:noreply, socket}
+  end
 end
+
+# Client code examples:
+
+# var phoenix = require("phoenix")
+
+# var socket = new phoenix.Socket("/socket", {})
+
+# socket.connect()
+
+# function new_channel(player, screen_name) {
+#     return socket.channel("game:" + player, {screen_name: screen_name});
+# }
+
+# function join(channel) {
+#     channel.join()
+#     .receive("ok", response => {
+#         console.log("Joined Successfully!", response)
+#     })
+#     .receive("error", response => {
+#         console.log("Unable to join", response)
+#     })
+# }
+
+# function leave(channel) {
+#     channel.leave()
+#     .receive("ok", response => { console.log("Left Successfully!", response)})
+#     .receive("error", response => { console.log("Unable to Leave", response)})
+# }
+
+# function say_hello(channel, greeting) {
+#     channel.push("hello", {"message": greeting})
+#     .receive("ok", response => { console.log("Hello", response.message)})
+#     .receive("error", response => { console.log("Unable to say hello to the channel.", response.message)})
+# }
+
+# var game_channel = new_channel("moon", "diva")
+
+# join(game_channel)
+
+# game_channel.on("said_hello", response => {
+#     console.log("Returned Greeting", response.message)
+# })
+
+
